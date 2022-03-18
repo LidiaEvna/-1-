4a0dfd197796c17736e310678c5fff9f19c35421
Лида <97759859+LidiaEvna@users.noreply.github.com>
2022-03-18 10:03:38 +0300

    init commit2

--- app/src/main/java/com/example/cookies/MainActivity.java
+++ app/src/main/java/com/example/cookies/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cookies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,12 +11,33 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG ="MyApp";
     private Integer counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("count", counter);
+        Log.d(TAG, "onSaveInstanceState");
+    }
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null &&
+                savedInstanceState.containsKey("count")) {
+             counter = savedInstanceState.getInt("count");
+        }
+        Log.d(TAG, "onRestoreInstanceState");
+        resetUI();
+    }
+    private void resetUI() {
+        ((TextView) findViewById(R.id.txt_counter)).setText(counter.toString());
+        Log.d(TAG, "resetUI");
     }
     public void onClickBtnAddStudents(View view) {
         counter++;
